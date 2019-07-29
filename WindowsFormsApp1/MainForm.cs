544c30744ce3b9eced09638877f0f3fed974aa81
Tyler Long <tyler4long@gmail.com>
2019-07-29 15:23:14 -0700

    Initial commit

---
+++ WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var oauthForm = new OAuthForm(this);
+            oauthForm.ShowDialog();
+        }
+
+        public void SetLabelTest(String labelText)
+        {
+            this.label1.Text = labelText;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/OAuthForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RingCentral;
+
+namespace WindowsFormsApp1
+{
+    public partial class OAuthForm : Form
+    {
+        private const string REDIRECT_URI = "https://does-not-exist.com";
+        private RestClient rc = new RestClient("", "", true);
+        private MainForm mainForm;
+        public OAuthForm(MainForm mainForm)
+        {
+            this.mainForm = mainForm;
+            InitializeComponent();
+
+            this.webBrowser1.Navigating += WebBrowser1_Navigating;
+
+            this.webBrowser1.Url = new Uri(rc.AuthorizeUri(REDIRECT_URI));
+        }
+
+        private async void WebBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            var url = e.Url.ToString();
+            if (url.StartsWith(REDIRECT_URI))
+            {
+                var code = url.Split(new string[] { "?code=" }, StringSplitOptions.None)[1];
+                this.Hide();
+                await rc.Authorize(code, REDIRECT_URI);
+                mainForm.SetLabelTest("You have been authorized, access token is " + rc.token.access_token);
+            }
+        }
+    }
+}
